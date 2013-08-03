9997287df6e17a62a6dc9088702597d615f428f9
Phil Bayfield <phil@bayfmail.com>
2013-08-03 22:11:53 +0100

    removed build files

--- libproject/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/phil/android-sdks/extras/google/google_play_services/libproject/google-play-services_lib/bin/AndroidManifest.xml : \
-/Users/phil/android-sdks/extras/google/google_play_services/libproject/google-play-services_lib/AndroidManifest.xml \
