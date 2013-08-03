0134e680566ef0ef8b90ebc9ed94ed62f75d1eaa
Phil Bayfield <phil@bayfmail.com>
2013-08-03 21:30:10 +0100

    initial commit

---
+++ libproject/google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/phil/android-sdks/extras/google/google_play_services/libproject/google-play-services_lib/bin/AndroidManifest.xml : \
+/Users/phil/android-sdks/extras/google/google_play_services/libproject/google-play-services_lib/AndroidManifest.xml \
